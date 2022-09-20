2045d3b39a2ae2f253f661b049ede3e70231baf7
footballnft <85647902+footballnft@users.noreply.github.com>
2022-09-20 11:55:40 +0100

    update uikit theme folder

--- packages/uikit/src/components/Card/theme.ts
+++ packages/uikit/src/components/Card/theme.ts
@@ -1,33 +1,33 @@
-import { vars } from "@pancakeswap/ui/css/vars.css";
 import { darkColors, lightColors } from "../../theme/colors";
+import { shadows } from "../../theme/base";
 import { CardTheme } from "./types";
 
 export const light: CardTheme = {
   background: lightColors.backgroundAlt,
-  boxShadow: vars.shadows.level1,
-  boxShadowActive: vars.shadows.active,
-  boxShadowSuccess: vars.shadows.success,
-  boxShadowWarning: vars.shadows.warning,
+  boxShadow: shadows.level1,
+  boxShadowActive: shadows.active,
+  boxShadowSuccess: shadows.success,
+  boxShadowWarning: shadows.warning,
   cardHeaderBackground: {
-    default: lightColors.gradientCardHeader,
-    blue: lightColors.gradientBlue,
-    bubblegum: lightColors.gradientBubblegum,
-    violet: lightColors.gradientViolet,
+    default: lightColors.gradients.cardHeader,
+    blue: lightColors.gradients.blue,
+    bubblegum: lightColors.gradients.bubblegum,
+    violet: lightColors.gradients.violet,
   },
   dropShadow: "drop-shadow(0px 1px 4px rgba(25, 19, 38, 0.15))",
 };
 
 export const dark: CardTheme = {
   background: darkColors.backgroundAlt,
-  boxShadow: vars.shadows.level1,
-  boxShadowActive: vars.shadows.active,
-  boxShadowSuccess: vars.shadows.success,
-  boxShadowWarning: vars.shadows.warning,
+  boxShadow: shadows.level1,
+  boxShadowActive: shadows.active,
+  boxShadowSuccess: shadows.success,
+  boxShadowWarning: shadows.warning,
   cardHeaderBackground: {
-    default: darkColors.gradientCardHeader,
-    blue: darkColors.gradientBlue,
-    bubblegum: lightColors.gradientBubblegum,
-    violet: darkColors.gradientViolet,
+    default: darkColors.gradients.cardHeader,
+    blue: darkColors.gradients.blue,
+    bubblegum: lightColors.gradients.bubblegum,
+    violet: darkColors.gradients.violet,
   },
   dropShadow: "drop-shadow(0px 1px 4px rgba(25, 19, 38, 0.15))",
 };
