ce2fdbc0c465cd6e113889e0085748c8dd22b429
footballnft <85647902+footballnft@users.noreply.github.com>
2022-10-29 13:11:32 +0100

    updates-49

--- src/views/Nft/market/components/BuySellModals/BuyModal/index.tsx
+++ src/views/Nft/market/components/BuySellModals/BuyModal/index.tsx
@@ -140,7 +140,7 @@ const BuyModal: React.FC<React.PropsWithChildren<BuyModalProps>> = ({ nftToBuy, 
       stage={stage}
       onDismiss={onDismiss}
       onBack={showBackButton ? goBack : null}
-      headerBackground={theme.colors.gradientCardHeader}
+      headerBackground={theme.colors.gradients.cardHeader}
     >
       {stage === BuyingStage.REVIEW && (
         <ReviewStage
