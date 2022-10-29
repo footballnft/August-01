05c13e1ac7446d3207dfc4cfa02be3fd4db03663
footballnft <85647902+footballnft@users.noreply.github.com>
2022-10-29 12:49:53 +0100

    updates-47

--- src/views/Nft/market/Home/index.tsx
+++ src/views/Nft/market/Home/index.tsx
@@ -16,7 +16,7 @@ import Newest from './Newest'
 import config from './config'
 
 const Gradient = styled(Box)`
-  background: ${({ theme }) => theme.colors.gradientCardHeader};
+  background: ${({ theme }) => theme.colors.gradients.cardHeader};
 `
 
 const StyledPageHeader = styled(PageHeader)`
