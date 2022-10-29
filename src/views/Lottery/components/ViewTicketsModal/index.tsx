277e3c351075f4363876a87f4101d38d58376007
footballnft <85647902+footballnft@users.noreply.github.com>
2022-10-29 12:40:10 +0100

    updates-46

--- src/views/Lottery/components/ViewTicketsModal/index.tsx
+++ src/views/Lottery/components/ViewTicketsModal/index.tsx
@@ -33,7 +33,7 @@ const ViewTicketsModal: React.FC<React.PropsWithChildren<ViewTicketsModalProps>>
     <StyledModal
       title={`${t('Round')} ${roundId}`}
       onDismiss={onDismiss}
-      headerBackground={theme.colors.gradientCardHeader}
+      headerBackground={theme.colors.gradients.cardHeader}
     >
       {isPreviousRound ? <PreviousRoundTicketsInner roundId={roundId} /> : <CurrentRoundTicketsInner />}
     </StyledModal>
